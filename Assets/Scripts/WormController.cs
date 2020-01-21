65528193bf72e830e28243e94d4e4d1c7d7162e3
Noah Hatz <Noah@Noah-Hatz-MacBook.local>
2020-01-21 15:05:07 +0100

    Start wrinitng script. Creating first few game objects (Worm/Ground)

---
+++ Assets/Scripts/WormController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+    public int x;
+    public string text;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (x > 0)
+        {
+            Debug.Log("x ist größer als 0");
+        }
+        else
+        {
+            Debug.Log("x ist nicht größer als 0");
+        }
+
+        if (text == "HATZ")
+        {
+            Debug.Log("Good Name, Boy!");
+        }
+        else
+        {
+            Debug.Log("Pls write good name");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
