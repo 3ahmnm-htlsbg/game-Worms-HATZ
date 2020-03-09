22811781b1900c5cf7867fcaa75055ac51103807
Noah Hatz <Noah@Noah-Hatz-MacBook.local>
2020-03-09 17:38:27 +0100

    Create Movements and Controls.

--- Assets/Scripts/WormController.cs
+++ Assets/Scripts/WormController.cs
@@ -1,46 +1,79 @@
-﻿using System.Collections;
+﻿using System.Security.Cryptography;
+using System.Runtime.ConstrainedExecution;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WormController : MonoBehaviour
 {
-    public int x;
-    public string text;
-    public int y;
-    
-    // Start is called before the first frame update
+  public float thrust = 1.0f;
+  public float negativeThrust = 1.0f;
+  public Rigidbody rb;
+  public Rigidbody b;
+  public Transform prefab;
+  public Transform spawnPosition;
+  public Rigidbody clone;
+
+  private void Update() {
+      if (Input.GetKeyDown(KeyCode.W)) {
+          rb.AddForce(0, thrust, 0, ForceMode.Impulse);
+      }
+
+     if(Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("Wird aufgerufen");
+            rb.AddForce(thrust, 0, 0, ForceMode.Impulse);
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Wird aufgerufen");
+            rb.AddForce(negativeThrust, 0, 0, ForceMode.Impulse);
+        }
+         if(Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Wird aufgerufen");
+            rb.AddForce(0, negativeThrust, 0, ForceMode.Impulse);
+        }
+          if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Wird aufgerufen");
+            clone = Instantiate(b, spawnPosition.position, transform.rotation);
+            clone.velocity = transform.TransformDirection(Vector3.left * -7);
+        }
+    }
+
+
     void Start()
     {
-        if (x > 0)
-        {
-            Debug.Log("x ist größer als 0");
-        }
-        else
-        {
-            Debug.Log("x ist nicht größer als 0");
-        }
-
-        if (text == "HATZ")
-        {
-            Debug.Log("Good Name, Boy!");
-        }
-        else
-        {
-            Debug.Log("Pls write good name");
-        }
-        if (y>10)
-        {
-            Debug.Log("Wert ist über 10");
-        }
-        else 
-        {
-            Debug.Log("Wert ist unter 10");
-        }
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public int health = 10;
+
+    void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.tag == "Bullet")
+        {
+           health = health -4;
+           Debug.Log ("Kollision mit Bullet findet statt"); 
+        }
         
+
+        else {
+            Debug.Log ("Kollision mit etwas anderem findet statt");
+        }
+
+        if (collision.gameObject.tag == "Health")
+        {
+           health = health +4;
+           Debug.Log ("Kollision mit HealthBox findet statt"); 
+        }
+        
+
+        else {
+            Debug.Log ("Kollision mit etwas anderem findet statt");
+        }
     }
+
 }
