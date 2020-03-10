678453d12fe52e22787c4430f27c2f62a5298092
Noah Hatz <Noah@Noah-Hatz-MacBook.local>
2020-03-10 15:02:10 +0100

    Finish UI and Game-Controlls.

--- Assets/Scripts/WormController.cs
+++ Assets/Scripts/WormController.cs
@@ -1,45 +1,56 @@
-﻿using System.Security.Cryptography;
+﻿using System.Net.Mime;
+using System.Security.Cryptography;
 using System.Runtime.ConstrainedExecution;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WormController : MonoBehaviour
 {
-  public float thrust = 1.0f;
-  public float negativeThrust = 1.0f;
-  public Rigidbody rb;
-  public Rigidbody b;
-  public Transform prefab;
-  public Transform spawnPosition;
-  public Rigidbody clone;
+    public float thrust = 1.0f;
+    public float negativeThrust = 1.0f;
+    public Rigidbody rb;
+    public Rigidbody b;
+    public Transform prefab;
+    public Transform spawnPosition;
+    public Rigidbody clone;
+    public Text text1;
+    public int health = 10;
 
-  private void Update() {
-      if (Input.GetKeyDown(KeyCode.W)) {
-          rb.AddForce(0, thrust, 0, ForceMode.Impulse);
-      }
+    public Vector3 shootSpeed;
 
-     if(Input.GetKeyDown(KeyCode.D))
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(0, thrust, 0, ForceMode.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
         {
             Debug.Log("Wird aufgerufen");
             rb.AddForce(thrust, 0, 0, ForceMode.Impulse);
         }
-        if(Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("Wird aufgerufen");
             rb.AddForce(negativeThrust, 0, 0, ForceMode.Impulse);
         }
-         if(Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S))
         {
             Debug.Log("Wird aufgerufen");
             rb.AddForce(0, negativeThrust, 0, ForceMode.Impulse);
         }
-          if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Wird aufgerufen");
             clone = Instantiate(b, spawnPosition.position, transform.rotation);
-            clone.velocity = transform.TransformDirection(Vector3.left * -7);
+            Rigidbody rigid = clone.GetComponent<Rigidbody>();
+            rigid.AddForce(shootSpeed);
         }
+
+        UpdateLive();
     }
 
 
@@ -48,32 +59,37 @@ public class WormController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-
-    public int health = 10;
-
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Bullet")
+        if (collision.gameObject.tag == "Bullet2")
         {
-           health = health -4;
-           Debug.Log ("Kollision mit Bullet findet statt"); 
+            health = health - 4;
+            Debug.Log("Kollision mit Bullet findet statt");
+            Destroy(collision.gameObject);
         }
-        
 
-        else {
-            Debug.Log ("Kollision mit etwas anderem findet statt");
+
+        else
+        {
+            Debug.Log("Kollision mit etwas anderem findet statt");
         }
 
         if (collision.gameObject.tag == "Health")
         {
-           health = health +2;
-           Debug.Log ("Kollision mit HealthBox findet statt"); 
+            health = health + 2;
+            Debug.Log("Kollision mit HealthBox findet statt");
         }
-        
 
-        else {
-            Debug.Log ("Kollision mit etwas anderem findet statt");
+
+        else
+        {
+            Debug.Log("Kollision mit etwas anderem findet statt");
         }
+    }
+
+    public void UpdateLive()
+    {
+        text1.text = health.ToString();
     }
 
 }
--- Assets/Scripts/WormControlls_2.cs
+++ Assets/Scripts/WormControlls_2.cs
@@ -1,46 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WormControlls_2 : MonoBehaviour
 {
-   public float thrust = 1.0f;
-  public float negativeThrust = 1.0f;
-  public Rigidbody rb;
-  public Rigidbody b;
-  public Transform prefab;
-  public Transform spawnPosition;
-  public Rigidbody clone;
+    public float thrust = 1.0f;
+    public float negativeThrust = 1.0f;
+    public Rigidbody rb;
+    public Rigidbody b;
+    public Transform prefab;
+    public Transform spawnPosition;
+    public Rigidbody clone;
+    public Text text2;
+    public Vector3 shootSpeed;
 
-  private void Update() {
-      if (Input.GetKeyDown(KeyCode.UpArrow)) {
-          rb.AddForce(0, thrust, 0, ForceMode.Impulse);
-      }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.AddForce(0, thrust, 0, ForceMode.Impulse);
+        }
 
-     if(Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             Debug.Log("Wird aufgerufen");
             rb.AddForce(thrust, 0, 0, ForceMode.Impulse);
         }
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             Debug.Log("Wird aufgerufen");
             rb.AddForce(negativeThrust, 0, 0, ForceMode.Impulse);
         }
-         if(Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("Wird aufgerufen");
             rb.AddForce(0, negativeThrust, 0, ForceMode.Impulse);
         }
-          if(Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Wird aufgerufen");
             clone = Instantiate(b, spawnPosition.position, transform.rotation);
-            clone.velocity = transform.TransformDirection(Vector3.left * 7);
+            Rigidbody rigid = clone.GetComponent<Rigidbody>();
+            rigid.AddForce(shootSpeed);
         }
+
+        UpdateLife();
     }
 
-       void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
@@ -52,24 +60,32 @@ public class WormControlls_2 : MonoBehaviour
     {
         if (collision.gameObject.tag == "Bullet")
         {
-           health = health -4;
-           Debug.Log ("Kollision mit Bullet findet statt"); 
+            health = health - 4;
+            Debug.Log("Kollision mit Bullet findet statt");
+            Destroy(collision.gameObject);
         }
-        
 
-        else {
-            Debug.Log ("Kollision mit etwas anderem findet statt");
+
+        else
+        {
+            Debug.Log("Kollision mit etwas anderem findet statt");
         }
 
         if (collision.gameObject.tag == "Health")
         {
-           health = health +2;
-           Debug.Log ("Kollision mit HealthBox findet statt"); 
+            health = health + 2;
+            Debug.Log("Kollision mit HealthBox findet statt");
         }
-        
 
-        else {
-            Debug.Log ("Kollision mit etwas anderem findet statt");
+
+        else
+        {
+            Debug.Log("Kollision mit etwas anderem findet statt");
         }
     }
+
+    public void UpdateLife()
+    {
+        text2.text = health.ToString();
     }
+}
